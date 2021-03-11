beb7b792530b96adbc385e4155cf8b12ea6a50d6
RHughes98 <ryan.hughes.md@gmail.com>
2021-03-11 09:44:06 -0700

    avg. and CC criteria, integrated into dataProcessingScript

--- PB_final_section.m
+++ PB_final_section.m
@@ -11,9 +11,20 @@ PB.avgShort = movmean(rate.rate5,PB.avgWindowShort,'Endpoints','fill');
 % shifted avg. for plotting
 % PB.shiftedAvg = 
 
-% criteria
-%  crit1 = 
-%  critCC = 
+% avg.-based criteria
+PB.critAvg = PB.avgShort > 1.2 * PB.avg;
+
+% correlation coefficient-based criteria
+PB.dataLength = length(PB.avg);
+PB.movCC = zeros(PB.dataLength,1);
+PB.CCwindow = 5*10; %5 s, running CC time window
+for i = (PB.CCwindow+1):PB.dataLength-(PB.CCwindow-1)
+    PB.tmpCC = corrcoef(PB.avgShort(i-PB.CCwindow:i+PB.CCwindow-1),...
+        PB.avg(i-PB.CCwindow:i+PB.CCwindow-1));
+    PB.movCC(i) = PB.tmpCC(2);
+end
+PB.critCC = PB.movCC < .955;
 
 % function calls
-% [PB.bandStart, PB.bandEnd] = mergedCritBands(
+% [PB.bandStart, PB.bandEnd] = PBands(PB.critAvg,PB.critCC,rate.rate5,5,[]);
+[PB.bandStart, PB.bandEnd] = mergedCritBands(PB.critAvg,PB.critCC,rate.rate5,5,[]);
