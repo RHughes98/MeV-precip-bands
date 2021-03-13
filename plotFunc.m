ff2690901493d4da3f95f725bea57dd6b72a41b3
RHughes98 <ryan.hughes.md@gmail.com>
2021-03-13 14:26:12 -0700

    suppressed interactions w/ PB2 and VA structs

--- plotFunc.m
+++ plotFunc.m
@@ -1,5 +1,5 @@
 function [] = plotFunc(t,rate,tShort,rateShort,bandStart,bandEnd,...
-    avgShort,crit1,crit2,MB,PB2,humps)
+    avgShort,crit1,crit2,MB)%,PB2,humps)
 
 % rate vs. time
 figure
@@ -8,7 +8,7 @@ hold on
 semilogy(MB.tShort(MB.burstIndex),MB.rateShort(MB.burstIndex),'r*','MarkerSize',7)
 semilogy(tShort(bandStart),rateShort(bandStart),'gd','MarkerSize',7)
 semilogy(tShort(bandEnd),rateShort(bandEnd),'ms','MarkerSize',7)
-semilogy(t.*humps,rate.*humps,'k')
+% semilogy(t.*humps,rate.*humps,'k')
 % semilogy(t,SAA*9000,"LineWidth",1.5)
 title("Count Rate vs. Time")
 xlabel("Time [h]"); ylabel("Count Rate (per 100ms, log-scaled)")
@@ -34,19 +34,19 @@ legend("20% baseline","2.5 second avg","Count rate","PB start",...
     "PB end","Criteria 1","Criteria 2")
 
 % compare PB algorithms
-figure
-semilogy(t,rate)
-hold on
-semilogy(t(bandStart),rate(bandStart),'gd','MarkerSize',7)
-semilogy(t(bandEnd),rate(bandEnd),'gs','MarkerSize',7)
-semilogy(tShort(PB2.lowStart),rateShort(PB2.lowStart),'md','MarkerSize',7)
-semilogy(tShort(PB2.lowEnd),rateShort(PB2.lowEnd),'ms','MarkerSize',7)
-semilogy(tShort(PB2.midStart),rateShort(PB2.midStart),'bd','MarkerSize',7)
-semilogy(tShort(PB2.midEnd),rateShort(PB2.midEnd),'bs','MarkerSize',7)
-title("Primary and Alternate Precip Bands")
-xlabel("Time [h]"); ylabel("Count rate (per 100 ms)")
-legend("Count rate", "PB start", "PB end", "short PB start", "short PB end",... 
-    "mid PB start", "mid PB end");
+% figure
+% semilogy(t,rate)
+% hold on
+% semilogy(t(bandStart),rate(bandStart),'gd','MarkerSize',7)
+% semilogy(t(bandEnd),rate(bandEnd),'gs','MarkerSize',7)
+% semilogy(tShort(PB2.lowStart),rateShort(PB2.lowStart),'md','MarkerSize',7)
+% semilogy(tShort(PB2.lowEnd),rateShort(PB2.lowEnd),'ms','MarkerSize',7)
+% semilogy(tShort(PB2.midStart),rateShort(PB2.midStart),'bd','MarkerSize',7)
+% semilogy(tShort(PB2.midEnd),rateShort(PB2.midEnd),'bs','MarkerSize',7)
+% title("Primary and Alternate Precip Bands")
+% xlabel("Time [h]"); ylabel("Count rate (per 100 ms)")
+% legend("Count rate", "PB start", "PB end", "short PB start", "short PB end",... 
+%     "mid PB start", "mid PB end");
 
 
 end
